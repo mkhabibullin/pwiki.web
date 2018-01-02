e3a7d39d520c987f67e2df34845f763a208fc0e4
KhabibullinM <marat-011@yandex.ru>
2018-01-02 22:26:11 +0300

    +

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -7,10 +7,26 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  http: HttpClient;
+  baseUrl: string;
+
   public notes: Note[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    this.http = http;
+    this.baseUrl = baseUrl;
     http.get<Note[]>(baseUrl + 'notes').subscribe(result => {
+      this.notes = result;
+    }, error => console.error(error));
+  }
+
+  public onSearch(e) {
+    this.http.get<Note[]>(
+      this.baseUrl + 'notes', {
+      params: {
+        filter: e
+      }}).subscribe(result => {
       this.notes = result;
     }, error => console.error(error));
   }
--- ClientApp/src/app/material.module.ts
+++ ClientApp/src/app/material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatCardModule, MatGridListModule, MatChipsModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule, MatCardModule, MatGridListModule, MatChipsModule, MatToolbarModule,
+  MatInputModule} from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatCardModule, MatGridListModule, MatChipsModule, MatToolbarModule],
-  exports: [MatButtonModule, MatCardModule, MatGridListModule, MatChipsModule, MatToolbarModule],
+  imports: [MatButtonModule, MatCardModule, MatGridListModule, MatChipsModule, MatToolbarModule, MatInputModule],
+  exports: [MatButtonModule, MatCardModule, MatGridListModule, MatChipsModule, MatToolbarModule, MatInputModule],
 })
 export class MaterialModule { }
